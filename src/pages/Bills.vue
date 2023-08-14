e457dc7e65fb13815043f1c163479e52021d3392
liz <lissete.trejo@gmail.com>
2023-08-14 14:37:15 -0700

    basic calendar dynamic functionality

--- src/pages/Bills.vue
+++ src/pages/Bills.vue
@@ -1,30 +1,49 @@
 <template>
-  <h1>Bills</h1>
+  <h1>Calendar</h1>
+  <n-input v-model:value="tagValue" type="text" placeholder="Event Name" />
+  <n-date-picker v-model:value="dateValue" type="date" />
+  <n-button
+    type="primary"
+    @click="addSingleDate(dateValue)"
+  >Add Date</n-button>
+
   <n-calendar
-    v-model:value="value"
-    #="{ year, month, date }"
-    :is-date-disabled="isDateDisabled"
-    @update:value="handleUpdateValue"
   >
-    {{ year }}-{{ month }}-{{ date }}
+    <template #default="{ year, month, date }">
+      <div class="flex flex-col gap-1">
+        <template v-for="item in dates" >
+          <n-tag
+          v-if="year === item.year && month === item.month && date === item.day" >
+            {{ item.tag }}
+          </n-tag>
+        </template>
+      </div>
+    </template>
   </n-calendar>
 </template>
-
-<script setup>
+`<script setup>
 import { ref } from "vue";
-import { useMessage } from 'naive-ui'
-import { isYesterday, addDays } from 'date-fns/esm'
+import { NCalendar, NTag } from "naive-ui";
+import { addDays } from 'date-fns/esm'
 
-const message = useMessage();
+const dateValue = ref(null)
+const tagValue = ref(null)
+const dates = ref([]);
 
-const value = ref(addDays(Date.now(), 1).valueOf());
-const handleUpdateValue = (_, { year, month, date }) => {
-  message.success(`${year}-${month}-${date}`);
+const getYearMonthDate = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return { year, month, day };
 };
-const isDateDisabled = (timestamp) =>{
-  if (isYesterday(timestamp)) {
-    return true;
-  }
-  return false;
-}
+
+const addSingleDate = (timestamp) => {
+  const dateObj = getYearMonthDate(new Date(timestamp));
+  dates.value.push({
+    ...dateObj,
+    tag: tagValue.value,
+  })
+};
+
+const value = ref(addDays(Date.now(), 0).valueOf());
 </script>
