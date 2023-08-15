e2fab1ba95768bad1442dd773085bbdf27596584
liz <lissete.trejo@gmail.com>
2023-08-14 18:31:34 -0700

    complete event functionality, including edit and delete

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -1,17 +1,6 @@
 <template>
   <h1>Events</h1>
-  <n-date-picker v-model:value="dateValue" type="date" />
-  <div class="flex">
-    <n-input v-model:value="tagValue" type="text" placeholder="Event Name" />
-    <n-select v-model:value="typeValue" :options="typeOptions" :render-tag="renderTag" class="w-40"/>
-  </div>
-  <n-button
-    type="primary"
-    @click="addSingleDate"
-  >Add Date</n-button>
-
-  <n-calendar
-  >
+  <n-calendar class="mb-10">
     <template #default="{ year, month, date }">
       <div class="flex flex-col gap-1">
         <template v-for="item in dates">
@@ -26,21 +15,30 @@
                 <Icon icon="material-symbols:settings" />
               </n-button>
             </n-dropdown>
-
-            <!-- <n-dropdown :options="dropDownOptions.map(option => ({ ...option, id: item._id }))" @select="handleDropdownSelection"> -->
-
-            <!-- <n-dropdown trigger="hover" :options="dropDownOptions" @select="handleDropdownSelection"> -->
           </div>
         </template>
       </div>
     </template>
   </n-calendar>
+  <n-button @click="handleAdd" v-if="!showForm" class="mb-10" type="primary">
+    Add New Event
+  </n-button>
+  <n-card class="flex mb-8 max-w-2xl" v-if="showForm">
+    <h2 class="text-base font-semibold mt-0">{{ title }}</h2>
+    <n-input-group>
+      <n-date-picker v-model:value="dateValue" type="date" class="w-80"/>
+      <n-input v-model:value="tagValue" type="text" placeholder="Event Name" />
+      <n-select v-model:value="typeValue" :options="typeOptions" :render-tag="renderTag" class="w-64" placeholder="Type"/>
+      <n-button type="primary" @click="handleSave">Save</n-button>
+      <n-button @click="handleCancel">Cancel</n-button>
+    </n-input-group>
+  </n-card>
 </template>
 `<script setup>
 import { useAuth0 } from '@auth0/auth0-vue';
 import { ref, h, onMounted } from "vue";
 import { Icon } from '@iconify/vue';
-import { NCalendar, NTag, NDropdown, NButton, NInput, NSelect, NDatePicker } from "naive-ui";
+import { NCalendar, NTag, NDropdown, NButton, NInput, NSelect, NDatePicker, useMessage, NCard, NInputGroup } from "naive-ui";
 import { addDays } from 'date-fns/esm'
 import axios from 'axios';
 
@@ -50,8 +48,19 @@ const { user } = useAuth0();
 const dateValue = ref(null)
 const tagValue = ref(null)
 const typeValue = ref(null)
+const editingId = ref(null);
+const title = ref("Add New Event");
+const showForm = ref(false);
 const dates = ref([]);
 const userId = user._rawValue.sub;
+const message = useMessage();
+
+const clearInputs = () => {
+  editingId.value = null;
+  dateValue.value = null;
+  tagValue.value = null;
+  typeValue.value = null;
+};
 
 const renderIcon = icon => () => h(Icon, { icon });
 const renderTag = ({ option }) => {
@@ -83,17 +92,30 @@ const dropDownOptions = [
     key: "delete",
   },
 ];
+
+const handleAdd = () => {
+  clearInputs();
+  title.value = "Add New Event";
+  showForm.value = true;
+};
+
+const handleEdit = (id) => {
+  const index = dates.value.findIndex((item) => item._id === id);
+  const { year, month, day, tag, type } = dates.value[index];
+  dateValue.value = new Date(year, month - 1, day);
+  tagValue.value = tag;
+  typeValue.value = type;
+  editingId.value = id;
+  title.value = "Edit Event";
+  showForm.value = true;
+};
+
 const handleDropdownSelection = (key, id) => {
   const index = dates.value.findIndex((item) => item._id === id);
-
   if (key === "delete") {
-    dates.value.splice(index, 1);
+    handleDelete(id);
   } else {
-    console.log(dates.value[index])
-   dates.value[index] = {
-    ...dates.value[index],
-    tag: "New Tag",
-   }
+    handleEdit(id);
   }
 };
 
@@ -104,17 +126,61 @@ const getYearMonthDate = (date) => {
   return { year, month, day };
 };
 
-const addSingleDate = () => {
+const addNewDate = (dateObj) => {
+  axios.post(`${baseURL}/events`, dateObj)
+    .then((res) => {
+      dates.value.push(res.data);
+      message.success(
+        `${res.data.tag} added successfully!`,
+        { duration: 5e3 }
+      );
+      handleCancel();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const editDate = (editedDateObj) => {
+  const index = dates.value.findIndex((item) => item._id === editingId.value);
+  axios.patch(`${baseURL}/events/${editingId.value}`, editedDateObj)
+    .then((res) => {
+      dates.value[index] = res.data;
+      message.success(
+        `${res.data.tag} updated successfully!`,
+        { duration: 5e3 }
+      );
+      handleCancel();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const handleSave = () => {
   const dateObj = getYearMonthDate(new Date(dateValue.value));
-  const newSingleDate = {
+  const editedDateObj = {
     ...dateObj,
     tag: tagValue.value,
     type: typeValue.value,
-    user: userId,
   }
-  axios.post(`${baseURL}/events`, newSingleDate)
+  if (title.value === "Add New Event") {
+    addNewDate({...editedDateObj, user: userId});
+  } else {
+    editDate(editedDateObj);
+  }
+  
+};
+
+const handleDelete = (id) => {
+  axios.delete(`${baseURL}/events/${id}`)
     .then((res) => {
-      dates.value.push(res.data);
+      const index = dates.value.findIndex((item) => item._id === id);
+      message.success(
+        `${dates.value[index].tag} deleted successfully!`,
+        { duration: 5e3 }
+      );
+      dates.value.splice(index, 1);
     })
     .catch((err) => {
       console.log(err);
@@ -129,6 +195,11 @@ const getAllEvents = () => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+const handleCancel = () => {
+  clearInputs();
+  showForm.value = false;
 };
 
 onMounted(getAllEvents);
